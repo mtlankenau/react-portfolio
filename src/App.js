bb34dc83ccb88e78f003eb6acd71c359ce8a4024
mtlankenau <mtlankenau@gmail.com>
2022-01-23 14:57:27 -0500

    added basic footer to Footer component. Added Footer component to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header></Header>
       <main></main>
+      <Footer></Footer>
     </div>
   );
 }
