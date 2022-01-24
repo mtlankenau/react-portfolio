f864b8d6babb19d1ed3f2931fed348a7d6b9826f
mtlankenau <mtlankenau@gmail.com>
2022-01-23 22:40:48 -0500

    broken link on 'Michael Lankenau'. fixed link to be relative to end URL of '/react-portfolio'

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,7 +11,7 @@ const Header = (props) => {
 
   return (
     <header className="d-flex align-items-center flex-wrap justify-content-center py-3 mb-4 border-bottom bg-light sticky-top">
-      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <a href="/react-portfolio" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <span className="fs-1">Michael Lankenau</span>
       </a>
       <Navigation
